b24bef1550e723ec184c70f56853a2e49aebdd5d
sachajw <32266273+sachajw@users.noreply.github.com>
2017-11-07 20:26:41 +0200

    Made changes to align with PEP8

--- media.py
+++ media.py
@@ -1,19 +1,23 @@
 """The Movie class that contains the details of a movie."""
 import webbrowser
 
+
 class Movie():
     """This class provides a way to store movie related information"""
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    # inir function
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        """Invokes the Movie class instance variables."""
+
+# init function
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    def show_trailer(self) :
-        """Plays the movie trailer in the web browser."""
-        webbrowser.open(self.trailer_youtube_url)
+
+def show_trailer(self):
+    """Plays the movie trailer in the web browser."""
+    webbrowser.open(self.trailer_youtube_url)
